0aa48533eb02e64788a5950a78bf0ec3970a745b
Igor R <42152297+IgorWDPH@users.noreply.github.com>
2023-01-22 23:42:17 +0200

    Add files via upload
    home page + base styles

---
+++ script.js
@@ -0,0 +1,19 @@
+/*document.addEventListener('DOMContentLoaded', (event) => {
+    setWindowMode();
+});
+
+window.addEventListener('resize', (event) => {
+    setWindowMode();
+});
+
+function setWindowMode() {    
+    if(window.innerWidth > window.innerHeight && !document.body.classList.contains('landscape')) {
+        document.body.classList.remove('portrait');
+        document.body.classList.add('landscape');
+        return;
+    }
+    if(window.innerWidth < window.innerHeight && !document.body.classList.contains('portrait')) {
+        document.body.classList.remove('landscape');
+        document.body.classList.add('portrait');
+    }
+}*/
